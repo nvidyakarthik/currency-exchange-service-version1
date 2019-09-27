12e55e9641096db3c8cf05e9863005e6594851e2
Vidya Nirmal <v.nirmal@gymnasticbodies.com>
2019-09-27 14:01:15 -0700

    Initial commit

---
+++ src/main/java/com/gymnasticbodies/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,18 @@
+package com.gymnasticbodies.microservices.currencyexchangeservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+
+        return new ExchangeValue(1000L,"from","to",BigDecimal.valueOf(Long.valueOf("65")));
+
+    }
+}
